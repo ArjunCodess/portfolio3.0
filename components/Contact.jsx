d0d402802df26dd601595137a84081a2f807cd2a
Arjun Vijay Prakash <137415649+ArjunCodess@users.noreply.github.com>
2024-03-03 05:00:19 +0000

    front page7

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -12,15 +12,15 @@ const Contact = () => {
                 >
                     <div>
                         <label htmlFor="email" className="text-neutral-500 text-xs font-InterBold uppercase font-bold my-5">Your Email</label>
-                        <input type="email" id="email" className="block p-3 w-full text-sm text-gray-900 bg-slate-50 dark:bg-neutral-800/20 rounded-lg border border-gray-300 dark:border-gray-600 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@example.com" required />
+                        <input type="email" id="email" className="block p-3 w-full text-sm text-gray-900 dark:bg-slate-50 dark:bg-neutral-800/20 rounded-lg border border-gray-300 dark:border-gray-600 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@example.com" required />
                     </div>
                     <div>
                         <label htmlFor="fullname" className="text-neutral-500 text-xs font-InterBold uppercase font-bold my-5">Full Name</label>
-                        <input type="text" id="fullname" className="block p-3 w-full text-sm text-gray-900 bg-slate-50 dark:bg-neutral-800/20 rounded-lg border border-gray-300 dark:border-gray-600 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="John Doe" required />
+                        <input type="text" id="fullname" className="block p-3 w-full text-sm text-gray-900 dark:bg-slate-50 dark:bg-neutral-800/20 rounded-lg border border-gray-300 dark:border-gray-600 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="John Doe" required />
                     </div>
                     <div className="sm:col-span-2">
                         <label htmlFor="message" className="text-neutral-500 text-xs font-InterBold uppercase font-bold my-5">Your Message</label>
-                        <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-slate-50 dark:bg-neutral-800/20 rounded-lg shadow-sm border border-gray-300 dark:border-gray-600 focus:ring-primary-500 focus:border-primary-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Can you help me ship this project?..."></textarea>
+                        <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 dark:bg-slate-50 dark:bg-neutral-800/20 rounded-lg shadow-sm border border-gray-300 dark:border-gray-600 focus:ring-primary-500 focus:border-primary-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Can you help me ship this project?..."></textarea>
                     </div>
 
                     <button type="submit" className="w-full justify-center gap-x-4 text-gray-900 bg-white hover:bg-slate-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-slate-800 dark:border-gray-700 dark:text-white dark:hover:bg-slate-700 me-2 mb-2">
